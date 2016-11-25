63acebd6121c6fd005d41a81a8d7c01bfc387880
Daniel <dringler@mail.uni-mannheim.de>
2016-11-25 11:52:34 +0100

    get refs:label in addition to instance
    get english rdfs:label properties for each instance

--- GetInstances/src/GetInstances.java
+++ GetInstances/src/GetInstances.java
@@ -3,6 +3,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,32 +27,96 @@ public class GetInstances {
 		//DBpedia files
 		String dbpediaInstanceTypesTransitive = "/Users/curtis/SeminarPaper_KG_files/DBpedia/instance_types_transitive_en.ttl"; //_sample
 		String dbpediaInstanceTypes = "/Users/curtis/SeminarPaper_KG_files/DBpedia/instance_types_en.ttl"; //_sample
+		String dbpediaLabels = "/Users/curtis/SeminarPaper_KG_files/DBpedia/labels_en.ttl"; //_sample
 		
 		// get all classes for DBpedia
 		HashSet<String> dboClasses = getDBpediaClasses();
 		System.out.println(dboClasses);
+		HashSet<String> allInstancesSet = new HashSet<String>();
+		HashSet<String> labeledInstancesSet = new HashSet<String>();
 		
 		try {
+		// GET ALL INSTANCES FOR ALL CLASSES
 			//create stream objects of the files
+			//http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html
 			Stream<String> itTransitive = Files.lines(Paths.get(dbpediaInstanceTypesTransitive));
 			Stream<String> it = Files.lines(Paths.get(dbpediaInstanceTypes));
 			
+			
 			// read files
-			Map<String, Set<String>>linesThatHaveDboClass =
+			Map<String, Set<String>> classInstances =
 					//Files.lines(Paths.get(fileName))	
 					Stream.concat(itTransitive, it)
 						.skip(1) //skip first row 
-						.filter(line -> containsClassName(line, dboClasses)) //check if line contains a className
-						//collect: group by className, set of all instance names
-						.collect(Collectors.groupingBy(line -> getClassName(line), Collectors.mapping(line -> getInstanceName(line), Collectors.toSet())));
+						.filter(line -> containsClassName(line, dboClasses, allInstancesSet)) //check if line contains a className
+						//collect: group by className (third argument), set of all instance names (first argument): instance a className
+						.collect(Collectors.groupingBy(line -> getO(line), Collectors.mapping(line -> getS(line), Collectors.toSet())));
 		
 			//System.out.println(linesThatHaveDboClass);
-			for (Entry<String, Set<String>> entry : linesThatHaveDboClass.entrySet()) {
+			/*for (Entry<String, Set<String>> entry : classInstances.entrySet()) {
 				System.out.println(entry.getKey() + ": " + entry.getValue().size() + " instances");
 				//write instances to disk
 				Path fileName = Paths.get("results/" + getClassNameOfURI(entry.getKey()) + "Instances.txt");
 				Files.write(fileName, entry.getValue(), Charset.forName("UTF-8"));
 			}
+			*/
+			
+			
+			System.out.println("allInstancesSet.size():" + allInstancesSet.size());
+			int instanceCount = 0;
+			for (Entry<String, Set<String>> entry : classInstances.entrySet()) {
+				instanceCount += entry.getValue().size();
+			}
+			System.out.println("instanceCount:" + instanceCount);
+			
+			
+		// GET ENGLISH INSTANCE LABELS	
+			Stream<String> labels = Files.lines(Paths.get(dbpediaLabels));
+			Map<String, String> instancesWithLabel =
+					//Stream.of(labels)
+					labels
+							.skip(1) //skip first row
+							.filter(line -> containsInstanceNameEn(line, classInstances, allInstancesSet, labeledInstancesSet))
+							.collect(Collectors.toMap(
+									line -> getS(line), 
+									line -> getLabel(getO(line))
+								));
+			
+			//System.out.println(instancesWithLabel);
+			int noLabelCounter = 0;
+			//combine the results
+			Map<String, Set<String>> classInstancesWithLabel = new HashMap<String, Set<String>>();
+			for (Entry<String, Set<String>> entry : classInstances.entrySet()) {
+				System.out.println(entry.getKey() + ": " + entry.getValue().size() + " instances");
+				Set<String> instanceWithLabel = new HashSet<String>();
+				//for each instance in class set
+				for (String instance : entry.getValue()) {
+					//add instance uri and english label
+					instanceWithLabel.add(instance + "\t" + instancesWithLabel.get(instance));
+					if (instancesWithLabel.get(instance) == null) {
+						noLabelCounter += 1;
+					}
+				}
+				//add map (instance, label) to class map
+				classInstancesWithLabel.put(entry.getKey(), instanceWithLabel);
+			}
+					
+			System.out.println("No english label found for "+ noLabelCounter + " instances");
+					
+			
+			//write to file
+			//http://stackoverflow.com/questions/2885173/how-to-create-a-file-and-write-to-a-file-in-java
+			//for (Entry<String, Set<String>> entry : classInstances.entrySet()) {
+			for (Entry<String, Set<String>> entry : classInstancesWithLabel.entrySet()) {
+				//write instances to disk
+				Path fileName = Paths.get("results/" + getClassNameOfURI(entry.getKey()) + "InstancesWithLabels.txt");		
+				Files.write(fileName, entry.getValue(), Charset.forName("UTF-8"));
+				
+			}
+		
+		
+			
+			
 		
 		} catch (IOException e) {
 			System.out.println("ERROR WHILE reading files");
@@ -60,24 +125,65 @@ public class GetInstances {
 			System.out.println("EXECUTION TIME: " +  ((System.nanoTime() - startTime)/1000000000) + " seconds." );
 		}
 	}
-	
-	 /**
-	  * Get class name of line (third argument of line)
+
+	/**
+	  * Check if line contains an instance name with english label
 	  * @param line
 	  * @returns className (String)
 	 */
-	private static String getClassName(String line) {
+	 private static boolean containsInstanceNameEn(String line,
+			Map<String, Set<String>> classInstances, HashSet<String> allInstancesSet, HashSet<String> labeledInstancesSet) {
+		 boolean containsInstanceName = false;
+		//get spo triple
+		String spo[] = getSPO(line);
+		
+		//check if instance was already labeled
+		if (!labeledInstancesSet.contains(spo[0])) {
+			// check if property is rdfs:label
+			if (spo[1].equals("<http://www.w3.org/2000/01/rdf-schema#label>")) {
+				//check if label is in english
+				//if (spo[2].contains("@en")) {
+					//check if instanceName is contained in one of the sets
+					if(allInstancesSet.contains(spo[0])){
+						labeledInstancesSet.add(spo[0]);
+						containsInstanceName = true;
+					}
+					//} else {
+					//	System.out.println("label is not english: " + line);
+					//}
+				//} else {
+					//System.out.println("property is not rdfs:label for: " + line);
+			}
+		}
+		return containsInstanceName;
+	}
+
+	/**
+	  * Get third argument of line
+	  * @param line
+	  * @returns spo[2] (String)
+	 */
+	private static String getO(String line) {
 		String spo[] = getSPO(line);
 		return spo[2];
 	}
 	/**
-	  * Get instance name of line (first argument of line)
+	  * Get first argument of line
 	  * @param line
-	  * @returns instanceName (String)
+	  * @returns spo[0] (String)
 	 */
-	private static String getInstanceName(String line) {
+	private static String getS(String line) {
 		String spo[] = getSPO(line);
 		return spo[0];
+	}
+	/**
+	  * Get label of string
+	  * @param o (string)
+	  * @returns substring without label
+	 */
+	private static String getLabel(String o) {
+		//LABEL_TO_KEEP@en\s
+		return o.substring(0, o.length()-4); //-4 due to whitespace created by getSPO in the end
 	}
 	
 	/**
@@ -86,7 +192,7 @@ public class GetInstances {
 	  * @param classes (HashSet containing all class names)
 	  * @returns boolean
 	 */
-	private static boolean containsClassName(String line, HashSet<String> dboClasses ) {
+	private static boolean containsClassName(String line, HashSet<String> dboClasses, HashSet<String> allInstancesSet) {
 		boolean containsClass = false;
 		//get spo triple
 		String spo[] = getSPO(line);
@@ -98,6 +204,8 @@ public class GetInstances {
 				String lineClassName = getClassNameOfURI(spo[2]);
 				//check if className is in classNameArray
 				if (dboClasses.contains(lineClassName)) {
+					//add instance to allInstancesSet 
+					allInstancesSet.add(spo[0]);
 					containsClass = true;
 				}		
 			}
@@ -121,7 +229,39 @@ public class GetInstances {
 	  * @returns triple-Array
 	 */
 	private static String[] getSPO(String line) {
-		String words[] = line.split("\\s+");
+		String[] words;
+		// replace quotes in object and split line to s,p,o triple
+		if (line.contains("\"")) {
+			String[] allWords = line.replace("\"", "").split("\\s+");
+			int wordCounter = 0;
+			String s = "";
+			String p = "";
+			String o = "";
+			for (String word : allWords) {
+				if (wordCounter == 0) {
+					s = word;
+				} else if (wordCounter == 1) {
+					p = word;
+				} else {
+					o = o + word + " ";
+				}	
+				if (word.contains("@en")) {
+					break;
+				}
+				wordCounter += 1;
+			}
+			
+			//
+			String[] preWords = {s,p,o};
+			words = preWords;
+			
+			
+		} else {
+			String[] preWords = line.split("\\s+");
+			words = preWords;
+		}
+		//for (String word : words)
+		//	System.out.println(word);
 		return words;
 	}
 
