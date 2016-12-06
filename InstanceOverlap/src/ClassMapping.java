b35b14b7f533fe5d02a62289d5a133025cd1785d
Daniel <dringler@mail.uni-mannheim.de>
2016-12-06 16:09:27 +0100

    update imports

--- InstanceOverlap/src/ClassMapping.java
+++ InstanceOverlap/src/ClassMapping.java
@@ -1,11 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 
 public class ClassMapping {
 	HashMap<String, HashMap<String, ArrayList<String>>> fullClassMap = new HashMap<String, HashMap<String, ArrayList<String>>>();
